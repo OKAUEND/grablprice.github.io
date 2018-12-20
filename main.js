e5121a59e771689609bedb61a0a21516a65f3201
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2018-12-20 16:29:06 +0900

    Keyupイベントの簡素化
    Keyupイベントの簡素化

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 window.addEventListener('load',function(){
 
+    const MSG_INPUT_TYPE = "0~9までの数字を入力してくれよな！";
+    const MSG02 = "";
+
+    const TAG_ERROR = "has-error";
+    const TAG_SUCCESS = "has-success";
+    const FORM_ERROR = "form-error";
+    const FORM_SUCCESS = "form-success";
+
+    const PRICE = 300;
+    const PRICE_MULTIPLE = 10;
+    const PRICE_TEN = 3000;
+
     this.document.getElementById("btn").onclick = function()
     {
         
     };
 
-    [].forEach.call(this.document.getElementsByClassName('vaild-AssetStone'),function(input)
+    var AssetStone = this.document.querySelector('.vaild-AssetStone');
+    AssetStone.onkeyup = function(e)
     {
-        input.addEventListener('keyup',function(node)
-       {
-        
         var text = this.value;
         var msg_node = MsgNode();
         IsValidCheck(text,this.classList,msg_node)
-       }) ;
-    });
+    };
+
+    //[].forEach.call(this.document.getElementsByClassName('vaild-AssetStone'),function(input)
+    //{
+    //    input.addEventListener('keyup',function(node)
+    //   {
+    //    
+    //    var text = this.value;
+    //    var msg_node = MsgNode();
+    //    IsValidCheck(text,this.classList,msg_node)
+    //   }) ;
+    //});
 
     function IsValidCheck(inputtext,assetDOMClass,msgDOMClass){
 
